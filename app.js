55b8bbf4ee186ae14ad93eb22d9050e0a00a13d8
cameronwark7 <47673892+cameronwark7@users.noreply.github.com>
2021-07-07 20:10:34 -0700

    added client folder

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.get('/api/v1/something', (req, res) => {
+    
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
