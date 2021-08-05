9edbb4034d843792e402e48158244b784538b213
cameronwark7 <47673892+cameronwark7@users.noreply.github.com>
2021-08-05 08:00:37 -0700

    added navbar

---
+++ front-end/react-flashcard-app/src/components/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return(
+        <div>
+            <div>Home</div>
+            <div>Manage Decks</div>
+            <div>Add Card</div>
+            <div>Log in</div>
+        </div>
+    )
+}
+
+export default Navbar;  
