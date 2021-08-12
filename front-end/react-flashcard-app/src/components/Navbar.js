62fe6258db3498e79bc4f6b666dd41051db66f17
cameronwark7 <47673892+cameronwark7@users.noreply.github.com>
2021-08-12 12:25:08 -0700

    removed text on navbar

--- front-end/react-flashcard-app/src/components/Navbar.js
+++ front-end/react-flashcard-app/src/components/Navbar.js
@@ -3,10 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return(
         <div>
-            <div>Home</div>
             <Link to="/">home</Link>
-
-            <div>Add Card</div>
             <Link to="/add-card">add card</Link>
         </div>
     )
