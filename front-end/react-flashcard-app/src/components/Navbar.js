26653393c9825e028baec81856b0bcda34c20ebb
cameronwark7 <47673892+cameronwark7@users.noreply.github.com>
2021-08-12 12:23:01 -0700

    added router links to navbar

--- front-end/react-flashcard-app/src/components/Navbar.js
+++ front-end/react-flashcard-app/src/components/Navbar.js
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return(
         <div>
             <div>Home</div>
-            <div>Manage Decks</div>
+            <Link to="/">home</Link>
+
             <div>Add Card</div>
-            <div>Log in</div>
+            <Link to="/add-card">add card</Link>
         </div>
     )
 }
 
-export default Navbar;  
+export default Navbar;
