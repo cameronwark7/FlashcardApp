820900b220ac98f451b19252f1b3410df9f06b52
cameronwark7 <47673892+cameronwark7@users.noreply.github.com>
2021-08-13 12:13:57 -0700

    tracked form fields with useState hooks

--- front-end/react-flashcard-app/src/components/AddCard.js
+++ front-end/react-flashcard-app/src/components/AddCard.js
@@ -1,21 +1,41 @@
+import { useState } from "react";
+
 const AddCard = () => {
+
+    const [deck, setDeck] = useState('default');
+    const [front, setFront] = useState('');
+    const [back, setBack] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const card = {deck, front, back};
+        console.log(card);
+    }
+
     return(
         <div>
             <h2>Add Card</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Deck: </label>
-                <select>
-                    <option>Default</option>
+                <select
+                    value={deck}
+                    onChange={(e) => setDeck(e.target.value)}
+                >
+                    <option value="default">Default</option>
                 </select>
 
                 <label>Front: </label>
                 <textarea
                     required
+                    value={front}
+                    onChange={(e) => setFront(e.target.value)}
                 ></textarea>
 
                 <label>Back: </label>
                 <textarea
                     required
+                    value={back}
+                    onChange={(e) => setBack(e.target.value)}
                 ></textarea>
 
                 <button>Add Card</button>
