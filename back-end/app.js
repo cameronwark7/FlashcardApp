8f32f9df2a5da3cdeffc5cdc91943aacf80d70d7
cameronwark7 <47673892+cameronwark7@users.noreply.github.com>
2021-08-14 14:34:35 -0700

    added mongodb connection string, changed require syntax to import, and created separate file for posts.

--- back-end/app.js
+++ back-end/app.js
@@ -1,14 +1,29 @@
-const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+
 const app = express();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
 
 app.use(cors());
+
+// every route inside of postRoutes is going to start with /posts
+app.use('/posts', postRoutes);
 
 // Body parser middleware
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+
+const CONNECTION_URL = 'mongodb+srv://flashcard-app:flashcard-app123@cluster0.rkik8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const port = process.env.PORT || 3001;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(port, () => console.log(`listening on port ${port}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
 
 // endpoint to add card to deck
 app.post('/api/v1/add-card', (req, res) => {
@@ -19,5 +34,4 @@ app.get('/api/v1/', (req, res) => {
     // get request
 });
 
-const port = process.env.PORT || 3001;
-app.listen(port, () => console.log(`listening on port ${port}`));
+
