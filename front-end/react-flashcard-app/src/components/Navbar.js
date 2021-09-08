5c9fa66646a992e69552528bf4116fa59ec44222
cameronwark7 <47673892+cameronwark7@users.noreply.github.com>
2021-09-08 04:03:52 -0700

    completed conditional rendering and redirects for google login & logout.

--- front-end/react-flashcard-app/src/components/Navbar.js
+++ front-end/react-flashcard-app/src/components/Navbar.js
@@ -1,23 +1,40 @@
-import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory, useLocation } from 'react-router';
 
 const Navbar = () => {
 
     // 'profile' set in auth.js reducer.
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const location = useLocation();
 
     useEffect(() => {
         const token = user?.token;
         setUser(JSON.parse(localStorage.getItem('profile')));
-    }, []);
+    }, [location]);
+
+    const logout = () => {
+        dispatch({ type: 'LOGOUT' });
+        history.push('/');
+        setUser(null);
+    }
 
     return(
         <div>
             <Link to="/">Home</Link>
-            <Link to="/add-card">Add Cards</Link>
-            <Link to="/study">Study</Link>
-            <Link to="/decks">Decks</Link>
-            <Link to="/login">Sign in</Link>
+            {user ? (
+                <>
+                <Link to="/add-card">Add Cards</Link>
+                <Link to="/study">Study</Link>
+                <Link to="/decks">My Decks</Link>
+                <button onClick={logout}>Logout</button>
+                </>
+            ) : (
+                <Link to="/login">Sign in</Link>
+            )}
         </div>
     )
 }
