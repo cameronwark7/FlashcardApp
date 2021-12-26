04e2e585df012bfbc9c20dbb76bb605b7345203e
cameronwark7 <47673892+cameronwark7@users.noreply.github.com>
2021-12-26 04:12:07 -0800

    randomized deck on rebuild

--- front-end/react-flashcard-app/src/components/StudyDeck.js
+++ front-end/react-flashcard-app/src/components/StudyDeck.js
@@ -25,17 +25,13 @@ const StudyDeck = () => {
     }, [decks]);
 
     const setRandomizedDeck = (array) => {
-        // randomize 'selectedDeck' and set it to 'selectedDeckRandom'
         let currentIndex = array.length, randomIndex;
 
-        // While there remain elements to shuffle...
         while (currentIndex != 0) {
 
-            // Pick a remaining element...
             randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex--;
 
-            // And swap it with the current element.
             [array[currentIndex], array[randomIndex]] = [
             array[randomIndex], array[currentIndex]];
         }
@@ -79,8 +75,10 @@ const StudyDeck = () => {
     }
 
     const rebuildDeck = () => {
-        setSelectedDeckRandom(selectedDeck);
-        console.log(selectedDeck)
+        const x = decks.filter(obj => {
+            return obj.name == deckName;
+        });
+        setRandomizedDeck(x[0].cards);
     }
 
     return(
