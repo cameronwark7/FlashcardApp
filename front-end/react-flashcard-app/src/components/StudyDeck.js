9b29fff3e272e32a50ba0c6fe2ca8ebcfa61bc8c
cameronwark7 <47673892+cameronwark7@users.noreply.github.com>
2021-12-26 04:06:50 -0800

    added random algorithm when setting randomized deck

--- front-end/react-flashcard-app/src/components/StudyDeck.js
+++ front-end/react-flashcard-app/src/components/StudyDeck.js
@@ -24,11 +24,24 @@ const StudyDeck = () => {
         }
     }, [decks]);
 
-    const setRandomizedDeck = (arr) => {
+    const setRandomizedDeck = (array) => {
         // randomize 'selectedDeck' and set it to 'selectedDeckRandom'
+        let currentIndex = array.length, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (currentIndex != 0) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            // And swap it with the current element.
+            [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        }
 
         // set useState variable
-        setSelectedDeckRandom(arr);
+        setSelectedDeckRandom(array);
     }
 
     const showButtonClick = () => {
