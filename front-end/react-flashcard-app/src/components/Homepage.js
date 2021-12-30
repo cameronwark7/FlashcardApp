b9748ccc9d1112edf79dad1f999ea9ba4208f1ea
cameronwark7 <47673892+cameronwark7@users.noreply.github.com>
2021-12-30 04:28:06 -0800

    retrieved decks in homepage useEffect

--- front-end/react-flashcard-app/src/components/Homepage.js
+++ front-end/react-flashcard-app/src/components/Homepage.js
@@ -1,4 +1,22 @@
+import React, { useEffect } from 'react';
+import { getDecks } from '../actions/decks';
+import { useDispatch } from 'react-redux';
+
 const Homepage = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem('profile'));
+        if (user != null) {
+        const email = user.result.email;
+        const obj = {
+            email
+        }
+        dispatch(getDecks(obj));
+        }
+    }, [dispatch]);
+
     return(
         <div>
             <h2>Homepage</h2>
