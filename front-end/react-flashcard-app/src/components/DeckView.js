3d275500463b2f5ebe323c23d27bd719bec60c7f
cameronwark7 <47673892+cameronwark7@users.noreply.github.com>
2022-01-01 10:50:22 -0800

    added validation to deckview component

--- front-end/react-flashcard-app/src/components/DeckView.js
+++ front-end/react-flashcard-app/src/components/DeckView.js
@@ -14,6 +14,8 @@ const DeckView = () => {
     const [selectedIndex, setSelectedIndex] = useState(null);
     const [front, setFront] = useState('');
     const [back, setBack] = useState('');
+    const [frontErrorMessage, setFrontErrorMessage] = useState('');
+    const [backErrorMessage, setBackErrorMessage] = useState('');
 
     useEffect(() => {
         if (decks.length != 0) {
@@ -46,24 +48,60 @@ const DeckView = () => {
     }
 
     const saveCard = async () => {
-        // copy the array without the reference
-        let selectedDeckClone = [...selectedDeck];
+        const isValid = validate();
+        if (isValid) {
+            // copy the array without the reference
+            let selectedDeckClone = [...selectedDeck];
 
-        // update the cloned deck
-        const newValues = { front, back }
-        selectedDeckClone[selectedIndex] = newValues;
+            // update the cloned deck
+            const newValues = { front, back }
+            selectedDeckClone[selectedIndex] = newValues;
 
-        // get all necessary values to update deck
-        const profile = localStorage.getItem('profile');
-        const obj = {
-            deckName,
-            newCards: selectedDeckClone,
-            email: JSON.parse(profile).result.email
+            // get all necessary values to update deck
+            const profile = localStorage.getItem('profile');
+            const obj = {
+                deckName,
+                newCards: selectedDeckClone,
+                email: JSON.parse(profile).result.email
+            }
+
+            // send updated deck to backend
+            await api.updateDeck(obj);
+            window.location.reload();
+        }
+    }
+
+    const validate = () => {
+        // copy the values of the current deck and remove the selected index
+        const deckCopy = [...selectedDeck];
+        deckCopy.splice(selectedIndex, 1);
+
+        let validationState = true;
+
+        // with the index removed check if there are any cards with the same front value as what the user entered.
+        const duplicates = deckCopy.filter(c => c.front == front);
+        if (duplicates.length > 0) {
+            setFrontErrorMessage('Duplicate card.');
+            validationState = false;
+        }
+        if (front.length == 0) {
+            setFrontErrorMessage('*Required');
+            validationState = false;
+        }
+        if (back.length == 0) {
+            setBackErrorMessage('*Required');
+            validationState = false;
+        }
+        if (front.length > 100) {
+            setFrontErrorMessage('Front must be between 1 and 100 characters.');
+            validationState = false;
+        }
+        if (back.length > 100) {
+            setBackErrorMessage('Back must be between 1 and 100 characters.');
+            validationState = false;
         }
 
-        // send updated deck to backend
-        await api.updateDeck(obj);
-        window.location.reload();
+        return validationState;
     }
 
     return(
@@ -85,18 +123,45 @@ const DeckView = () => {
                 w={'40%'}
                 >
                     <Text>Front:</Text>
+                    {frontErrorMessage ? 
+                    <>
+                    <Textarea
+                    value={front}
+                    placeholder='Front'
+                    isInvalid
+                    onChange={(e) => setFront(e.target.value)}
+                    ></Textarea>
+                    <div>{frontErrorMessage}</div>
+                    </> 
+                    : 
+                    <>
                     <Textarea
                     value={front}
                     placeholder='Front'
                     onChange={(e) => setFront(e.target.value)}
                     ></Textarea>
+                    </>}
 
                     <Text>Back:</Text>
+                    {backErrorMessage ? 
+                    <>
+                    <Textarea
+                    value={back}
+                    isInvalid
+                    placeholder='Back'
+                    onChange={(e) => setBack(e.target.value)}
+                    ></Textarea>
+                    <div>{backErrorMessage}</div>
+                    </>
+                    :
+                    <>
                     <Textarea
                     value={back}
                     placeholder='Back'
                     onChange={(e) => setBack(e.target.value)}
                     ></Textarea>
+                    </>}
+                    {frontErrorMessage ? <></> : <></>}
                     {selectedIndex != null ? <Button onClick={saveCard}>Save Changes</Button> : <Button isDisabled>Save Changes</Button>}
                     {selectedIndex != null ? <Button onClick={deleteCard}>Delete Card</Button> : <Button isDisabled>Delete Card</Button>}
                 </Box>
